7891989976afe9b0776646bfd331ba8e1fde34cf
kevinquillen <kevin@inclind.com>
2013-06-21 13:57:56 -0400

    We don't need this.

--- javascripts/custom/navigation.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  var footer = $('#footer-navigation');  // cache footer to a variable for performance
-  footer.delegate('li.inactive', 'click', function () {
-    footer.find('.active').toggleClass('active inactive');
-    $(this).toggleClass('active inactive');
-  });
-});
