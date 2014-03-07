bc6e49aa9c63fb2100e19a0ea82fc33c30d412fc
Kevin Quillen <kevin@inclind.com>
2014-03-07 11:08:53 -0500

    Restructure repo for debugging purposes.

---
+++ vagrant/manifests/default.pp
@@ -0,0 +1,42 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    
+file { '/home/vagrant/.bash_aliases':
+  ensure => 'present',
+  source => 'puppet:///modules/puphpet/dot/.bash_aliases',
+}
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core',
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'local.menhaden.historiclewes.org':
+  server_name   => 'local.menhaden.historiclewes.org',
+  docroot       => '/var/www/menhaden.historiclewes.org/docroot',
+  port          => '80',
+  priority      => '1',
+}
